663e6b6c6ae5b2b35cc8eac3cb4b4dfd5cfad77f
Haru <frankcito639@gmail.com>
2022-09-10 15:01:45 -0500

    Subido

---
+++ 01-Node/homework/commands/index.js
@@ -0,0 +1,46 @@
+let fs = require("fs");
+let request = require("request");
+
+module.exports = {
+    echo: function(args, print){
+        print(args.join(" "));
+    },
+    date: function(args, print){
+        print(Date())
+    },
+    ls: function(args, print){
+        fs.readdir(".", function(err, files){
+            if(err) throw err;
+            print(files.join("\n"));
+        })
+    },
+    pwd: function(args, print){
+        print(process.cwd());
+    },
+    cat: function(args, print){
+        fs.readFile(args[0], "utf8", function(err, data){
+            if(err) throw err;
+            print(data);
+        })
+    },
+    head: function(args, print){
+        fs.readFile(args[0], "utf8", function(err, data){
+            if(err) throw err;
+            let lines = 10;
+            if(args[1]) lines = [args[1]];
+            print(data.split("\n").splice(0, lines).join("\n"));
+        })
+    },
+    tail: function(args, print){
+        fs.readFile(args[0], "utf8", function(err, data){
+            if(err) throw err;
+            print(data.split("\n").splice(-args[1]).join("\n"));   
+        })
+    },
+    curl: function(args, print){
+        request(args[0], function(err, data){
+            if(err) throw err;
+            print(data.body);
+        })
+    }
+}
