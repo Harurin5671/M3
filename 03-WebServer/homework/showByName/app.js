645bd5cad9f923b22bbf21c996bb6adbcf8bfd20
Haru <frankcito639@gmail.com>
2022-09-11 16:14:27 -0500

    actualizado

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -2,3 +2,15 @@ var fs  = require("fs")
 var http  = require("http")
 
 // Escribí acá tu servidor
+http.createServer((req, res) => {
+    fs.readFile(`${__dirname}/images/${req.url}`,(err, data) => {
+        if(err) {
+            res.writeHead(400,{"Context-Type":"text/plain"});
+            res.end("hubo un error");
+        }
+        else {
+            res.writeHead(200,{"Context-Type":"image/jpg"});
+            res.end(data);
+        }
+    })
+}).listen(3000, "127.0.0.1")
